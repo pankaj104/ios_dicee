ac54bd6d856b0c49cfdd7afb350d0b69e99066c4
pankaj104 <pankajkumar1122111@gmail.com>
2023-10-13 13:02:14 +0530

    first commit

---
+++ dicee-SwiftUI/dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  dicee_SwiftUIApp.swift
+//  dicee-SwiftUI
+//
+//  Created by Pankaj Kumar on 12/09/22.
+//
+
+import SwiftUI
+
+@main
+struct dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
